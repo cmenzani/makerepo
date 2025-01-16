f3cbe4e87b315da6c0668cd20f67f5c4cb26eb88
Claudio Menzani <cmenzani@vivido.it>
2025-01-16 15:06:15 +0100

    Update makevideo2audio.py

--- makevideo2audio.py
+++ makevideo2audio.py
@@ -18,5 +18,8 @@ def download():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
